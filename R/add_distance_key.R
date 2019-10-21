b941e75ef94bf055b05db3bae690ed46c3c6293e
Kristen Dybala <kdybala@gmail.com>
2019-10-21 09:25:29 -0700

    separate add_distance_key from compile_PC_dat, and add importfrom rlang .data

---
+++ R/add_distance_key.R
@@ -0,0 +1,27 @@
+#' Convert Distance Bins to numeric mindist and maxdist fields
+#'
+#' @param dat Dataframe containing the field 'Distance Bin'
+#'
+#' @return Dataframe
+#' @export
+#' @importFrom rlang .data
+#' @examples
+add_distance_key <- function(dat) {
+  key <- dat %>%
+    select(.data$Protocol, .data$`Distance Bin ID`, .data$`Distance Bin`) %>%
+    distinct() %>%
+    mutate(mindist = case_when(grepl('>', `Distance Bin`) ~
+                                 gsub('>', '', `Distance Bin`),
+                               grepl('<', `Distance Bin`) ~ '0',
+                               grepl('\\sto\\s', `Distance Bin`) ~
+                                 gsub(' to .*$', '', `Distance Bin`),
+                               `Distance Bin` == 'FlyOver' ~ NA_character_),
+           maxdist = case_when(grepl('>', `Distance Bin`) ~ NA_character_,
+                               grepl('<', `Distance Bin`) ~
+                                 gsub('<', '', `Distance Bin`),
+                               grepl('\\sto\\s', `Distance Bin`) ~
+                                 gsub('^.* to ', '', `Distance Bin`),
+                               `Distance Bin` == 'FlyOver' ~ NA_character_)) %>%
+    mutate_at(vars(.data$mindist:.data$maxdist), as.numeric)
+  left_join(dat, key, by = c('Protocol', 'Distance Bin ID', 'Distance Bin'))
+}
---
+++ R/compile_PC_dat.R
@@ -0,0 +1,41 @@
+#' Compile point count survey data from a directory
+#'
+#' @param dir directory containing point count survey data downloaded from CADC
+#' @param pattern regex pattern for identifying files to include (see \code{\link[base]{list.files}})
+#'
+#' @return Dataframe of compiled data, excluding any rows with all NA values.
+#' @details Also tests for missing values in critical fields.
+#' @export
+#' @import tidyr dplyr
+#' @importFrom purrr map_dfr
+#' @importFrom testthat test_that expect_false
+#' @importFrom rlang .data
+
+compile_PC_dat <- function(dir, pattern = '.csv') {
+  # get names of all files in directory that match pattern
+  file_list <- list.files(here::here(dir), pattern = pattern,
+                          full.names = TRUE)
+
+  # compile master data set (assuming CADC Project Leader format)
+  dat <- map_dfr(file_list,
+                 ~read_csv(.x,
+                           col_types = 'cccccnDtttncnccccccclcccc'))
+  dat <- dat %>%
+    filter_all(any_vars(!is.na(.dat))) %>%
+    mutate_at(vars(.data$Project:.data$Protocol,
+                   .data$`Time Bin`,
+                   .data$Spp:.data$`Breeding Status`,
+                   .data$Researcher:.data$`Data Status`),
+              as.factor)
+
+  testthat::test_that('Missing values in critical fields', {
+    testthat::expect_false(any(is.na(dat$Project)))
+    testthat::expect_false(any(is.na(dat$`Study Area`)))
+    testthat::expect_false(any(is.na(dat$Transect)))
+    testthat::expect_false(any(is.na(dat$Point)))
+    testthat::expect_false(any(is.na(dat$Protocol)))
+    testthat::expect_false(any(is.na(dat$Spp)))
+    testthat::expect_false(any(is.na(dat$`Distance Bin ID`)))
+    testthat::expect_false(any(is.na(dat$Count)))
+  })
+}
