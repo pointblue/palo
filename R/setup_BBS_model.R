1cc5fa89a8456d7714ce7ad2f5fc4d30268760de
Kristen Dybala <kdybala@gmail.com>
2019-10-22 16:05:18 -0700

    clarify documentation, factor levels, and add effort variable

--- R/setup_BBS_model.R
+++ R/setup_BBS_model.R
@@ -1,9 +1,7 @@
 #' Create input data for fitting BBS-style hierarchical model for estimating trends in point count survey data
 #'
 #' @param dat Dataframe or tibble containing point count data
-#' @param count Option allowing users to choose between modeling the maximum
-#' count on any visit to a point in a given year ('count_max'), or the mean
-#' across all visits ('count_mean').
+#' @param count Character string giving the name of the field containing the count data to be modeled.
 #'
 #' @return A list containing all inputs necessary for fitting BBS-style hierarchical model.
 #' @export
@@ -11,7 +9,10 @@
 #' @importFrom rlang .data
 #'
 setup_BBS_model <- function(dat, count = 'count_max') {
-  sdat <- dat %>% arrange(.data$Transect, .data$Point, .data$Year)
+  sdat <- dat %>% arrange(.data$Transect, .data$Point, .data$Year) %>%
+    mutate(Transect = factor(.data$Transect, levels = unique(.data$Transect)),
+           Point = factor(.data$Point, levels = unique(.data$Point)))
+
   year.pred <- seq(min(sdat$Year), max(sdat$Year), 1)
   prop <- sdat %>%
     group_by(.data$Transect, .data$Year) %>%
@@ -29,6 +30,7 @@ setup_BBS_model <- function(dat, count = 'count_max') {
     year.pred = year.pred,
     zyear.pred = (year.pred - min(sdat$Year)), #values to predict for
     prop = prop %>% pull(prop),
+    effort = sdat$n_visits,
 
     # transect, point, year ID numbers associated with each Count:
     transect = sdat$Transect %>% as.numeric(),
