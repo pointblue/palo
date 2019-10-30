fb23be3f3f9101b6a4f5646102dca5ee4fba673d
Kristen Dybala <kdybala@gmail.com>
2019-10-30 16:35:04 -0700

    add nprojects and project indices; convert prop to a matrix format; remove option for varying count field name

--- R/setup_BBS_model.R
+++ R/setup_BBS_model.R
@@ -1,43 +1,54 @@
-#' Create input data for fitting BBS-style hierarchical model for estimating trends in point count survey data
+#' Create input data for fitting a BBS-style hierarchical model for estimating
+#' trends in point count survey data
 #'
-#' @param dat Dataframe or tibble containing point count data
-#' @param count Character string giving the name of the field containing the count data to be modeled.
+#' @param dat Dataframe created by \code{\link{summarize_PC_dat}}
 #'
-#' @return A list containing all inputs necessary for fitting BBS-style hierarchical model.
+#' @return A list containing all inputs necessary for fitting BBS-style
+#' hierarchical model.
 #' @export
 #' @importFrom dplyr %>%
 #' @importFrom rlang .data
 #'
-setup_BBS_model <- function(dat, count = 'count_max') {
-  sdat <- dat %>% arrange(.data$Transect, .data$Point, .data$Year) %>%
-    mutate(Transect = factor(.data$Transect, levels = unique(.data$Transect)),
+setup_BBS_model <- function(dat) {
+  sdat <- dat %>%
+    arrange(.data$Project, .data$Transect, .data$Point, .data$Year) %>%
+    mutate(Project = factor(.data$Project, levels = unique(.data$Project)),
+           Transect = factor(.data$Transect, levels = unique(.data$Transect)),
            Point = factor(.data$Point, levels = unique(.data$Point)))
 
   year.pred <- seq(min(sdat$Year), max(sdat$Year), 1)
+
+  # proportion of transects within a project on which the species is present
+  # in each year
   prop <- sdat %>%
-    group_by(.data$Transect, .data$Year) %>%
-    summarize(present = sum(.data$count_mean)) %>%
+    group_by(.data$Project, .data$Transect, .data$Year) %>%
+    summarize(present = sum(.data$Count)) %>%
     mutate(present = ifelse(.data$present > 0, 1, 0)) %>%
-    group_by(.data$Year) %>%
+    group_by(.data$Project, .data$Year) %>%
     summarize(n = length(.data$Transect),
               n_present = sum(.data$present),
               prop = .data$n_present / n) %>%
-    ungroup()
+    ungroup() %>%
+    select(-n, -n_present) %>%
+    spread(key = Project, value = prop) %>%
+    as.matrix()
 
   list(
     observed = sdat %>% pull(!!count),
     zyear = sdat$Year - min(sdat$Year), #relative to baseline year
     year.pred = year.pred,
     zyear.pred = (year.pred - min(sdat$Year)), #values to predict for
-    prop = prop %>% pull(prop),
+    prop = prop,
     effort = sdat$n_visits,
 
-    # transect, point, year ID numbers associated with each Count:
+    # project, transect, point, year ID numbers associated with each Count:
+    project = sdat$Project %>% as.numeric(),
     transect = sdat$Transect %>% as.numeric(),
     point = sdat$Point %>% as.numeric(),
     year = sdat$Year %>% as.factor() %>% as.numeric(),
 
     # number of unique IDs for each:
+    nprojects = sdat$Project %>% unique() %>% length(),
     ntransects = sdat$Transect %>% unique() %>% length(),
     npoints = sdat$Point %>% unique() %>% length(),
     nyears = sdat$Year %>% unique() %>% length(),
