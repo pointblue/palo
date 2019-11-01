16d6cfa3a59f62eac2e3798438cd8c452418ab38
Kristen Dybala <kdybala@gmail.com>
2019-11-01 13:16:47 -0700

    update to equal-tailed CI and to support slope estimates; require direct input of projects and years

--- R/get_BBS_model_estimates.R
+++ R/get_BBS_model_estimates.R
@@ -1,35 +1,44 @@
 #' Extract annual trend or abundance index estimates from BBS-style hierarchical model
 #'
 #' @param modresults Model output from running \code{\link{fit_BBS_model}}
-#' @param inputdat Input data from running \code{\link{setup_BBS_model}}
 #' @param type Type of estimates to extract: annual indices of abundance
 #' ('index'), or predicted values from the linear trend ('trend').
-#' @details This function is automatically called by \code{\link{plot_BBS_model}},
-#' but may be useful for generating customized plots.
+#' @param projects Character string for the 4-letter project code(s), in all caps
+#' @param years Numeric vector for 4-digit years. See details.
 #'
-#' @return Dataframe containing predicted value and HDI for each year present
-#' in inputdat$year.pred (created by \code{\link{setup_BBS_model}})
+#' @details The BBS model output does not explicitly keep track of which years
+#' are included in each model, but keeps them in order. For type = 'index',
+#' provide a vector of years that are actually included in the original data.
+#' For type = 'trend', provide the full list of years between the earliest and last.
+#' @return Dataframe containing predicted value and 95% credible interval for each year
 #' @export
 #'
-get_BBS_model_estimates <- function(modresults, inputdat, type) {
+get_BBS_model_estimates <- function(modresults, type, projects = NULL, years = NULL) {
 
-  ci <- MCMCvis::MCMCpstr(modresults, params = type,
-                          func = function(x) HDInterval::hdi(x, .95))[[1]]
-  dimnames(ci)[[2]] <- levels(as.factor(as.character(inputdat$dat$Project)))
+  est <- MCMCvis::MCMCpstr(modresults, params = type,
+                          func = function(x) {
+                            quantile(x, probs = c(0.5, 0.025, 0.975))
+                          })[[1]]
 
-  med <- MCMCvis::MCMCpstr(modresults, params = type, func = median)[[1]]
-  names <- levels(as.factor(as.character(inputdat$dat$Project)))
-  if (length(names) == 1) {names <- list(names)}
-  dimnames(med)[[2]] <- names
+  if (type == 'slope') {
+    dimnames(est)[[2]] <- c('median', 'lower', 'upper')
+    res <- as.data.frame(est) %>%
+      mutate(type = type)
 
-  res <- cbind(year.pred = inputdat$year.pred,
-               data.frame(med),
-               data.frame(ci)) %>%
-    gather(-.data$year.pred, key = 'variable', value = 'value') %>%
-    separate(.data$variable, into = c('Project', 'variable'), fill = 'right') %>%
-    mutate(variable = replace_na(.data$variable, 'median')) %>%
-    spread(key = .data$variable, value = .data$value) %>%
-    arrange(.data$Project, .data$year.pred)
+  } else if (type %in% c('index', 'trend')) {
+    dimnames(est)[[2]] <- projects
+
+    res <- as.data.frame(est) %>%
+      mutate(Year = years) %>%
+      pivot_longer(-Year, names_to = 'variable', values_to = 'value') %>%
+      separate(.data$variable, into = c('Project', 'variable'), sep = 4, fill = 'right') %>%
+      mutate(variable = case_when(variable == '.50%' ~ 'median',
+                                  variable == '.2.5%' ~ 'lower',
+                                  variable == '.97.5%' ~ 'upper')) %>%
+      pivot_wider(names_from = 'variable', values_from = 'value') %>%
+      arrange(.data$Project, .data$Year) %>%
+      mutate(type = type)
+  }
 
   return(res)
 }
