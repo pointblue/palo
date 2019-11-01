cc9818bef106a16a7473b91f04188f87cb32c0e6
Kristen Dybala <kdybala@gmail.com>
2019-11-01 13:18:04 -0700

    simplify options and require separately running get_BBS_model_estimates

--- R/plot_BBS_model.R
+++ R/plot_BBS_model.R
@@ -1,75 +1,55 @@
 #' Plot trend results from BBS-style hierarchical model for point count survey data
 #'
-#' @param modresults Model output from running \code{\link{fit_BBS_model}}
-#' @param inputdat Input data from running \code{\link{setup_BBS_model}}
 #' @param type Type of plot to produce: plots of annual indices of abundance
-#' ('index') or plots of the linear trend lines ('trend'), both ('both').
-#' Defaults to 'both'.
-#' @details Runs \code{\link{get_BBS_model_estimates}} and produces ggplot
+#' ('index') or plots of the linear trend lines ('trend'), both ('both'). Defaults to 'both'.
+#' @param df Summary of median, lower, and upper estimates from \code{\link{get_BBS_model_estimates}}
+#' @details Produces line and ribbon for trend estimates, and points with error bars for index estimates.
 #' @return returns ggplot
 #' @export
 #' @import ggplot2
 #' @importFrom dplyr %>%
-#' @importFrom MCMCvis MCMCpstr
-#' @importFrom HDInterval hdi
-#' @importFrom stats median
 #' @importFrom rlang .data
 #'
-plot_BBS_model <- function(modresults, inputdat,
-                           type = 'both') {
-  if (type == 'both') {
-    res <- get_BBS_model_estimates(modresults, inputdat, type = 'index')
-    res2 <- get_BBS_model_estimates(modresults, inputdat, type = 'trend')
+plot_BBS_model <- function(df, type = 'both') {
+  p <- ggplot(df) +
+    theme_classic() +
+    xlab(NULL) +
+    ylab('Abundance index')
 
-    p <- ggplot(res) +
-      geom_pointrange(aes(x = .data$year.pred,
+  if (type %in% c('both', 'trend')) {
+    p <- p +
+      geom_ribbon(data = df %>% filter(type == 'trend'),
+                  aes(x = .data$Year,
+                      ymin = .data$lower,
+                      ymax = .data$upper),
+                  alpha = 0.2, fill = 'blue') +
+      geom_line(data = df %>% filter(type == 'trend'),
+                aes(x = .data$Year,
+                    y = .data$median),
+                size = 1, color = 'blue')
+  }
+  if (type %in% c('both', 'index')) {
+    p <- p +
+      geom_pointrange(data = df %>% filter(type == 'index'),
+                      aes(x = .data$Year,
                           y = .data$median,
                           ymin = .data$lower,
                           ymax = .data$upper)) +
-      geom_line(aes(x = .data$year.pred,
+      geom_line(data = df %>% filter(type == 'index'),
+                aes(x = .data$Year,
                     y = .data$median),
-                size = 1) +
-      geom_ribbon(data = res2,
-                  aes(x = .data$year.pred,
-                      ymin = .data$lower,
-                      ymax = .data$upper),
-                  alpha = 0.2, fill = 'blue') +
-      geom_line(data = res2,
-                aes(x = .data$year.pred,
-                    y = .data$median,
-                    color = .data$Transect),
-                size = 1, color = 'blue') +
-      theme_classic() +
-      xlab(NULL) +
-      ylab('Abundance index')
-
-  } else {
-    res <- get_BBS_model_estimates(modresults, inputdat, type = type)
-
-    obsdat <- data.frame(Year = inputdat$dat$Year,
-                         Project = inputdat$dat$Project,
-                         Count = inputdat$observed)
-    p <- ggplot(obsdat) +
-      geom_jitter(aes(.data$Year, .data$Count),
-                  width = 0.1, height = 0.1,
-                  color = 'gray30', shape = 21, alpha = 0.5) +
-      geom_ribbon(data = res,
-                  aes(x = .data$year.pred,
-                      ymin = .data$lower,
-                      ymax = .data$upper),
-                  alpha = 0.2, fill = 'blue') +
-      geom_line(data = res,
-                aes(x = .data$year.pred,
-                    y = .data$median),
-                size = 1, color = 'blue') +
-      theme_classic() +
-      xlab(NULL) +
-      ylab('Abundance index')
+                size = 1)
   }
 
-  if (inputdat$nprojects > 1) {
-    p <- p + facet_wrap(~.data$Project)
-  }
+  # if (length(unique(df$species)) > 1) {
+  #   if (length(unique(df$Project)) > 1) {
+  #     p <- p + facet_wrap(~.data$species + .data$Project)
+  #   } else {
+  #     p <- p + facet_wrap(~.data$species)
+  #   }
+  # } else if (length(unique(df$Project)) > 1) {
+  #   p <- p + facet_wrap(~.data$Project)
+  # }
   print(p)
   return(p)
 }
