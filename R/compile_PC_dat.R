134d3a10976494521aeee8c44f79ec538656f25d
Kristen Dybala <kdybala@gmail.com>
2026-09-08 17:47:28 -0700

    update to base pipe operator; remove here dependency; remove testthat dependency in favor of a warning

--- R/compile_PC_dat.R
+++ R/compile_PC_dat.R
@@ -1,43 +1,38 @@
 #' Compile point count survey data from a directory
 #'
 #' @param dir Directory containing point count survey data downloaded from CADC
-#' @param pattern Regex pattern for identifying files to include (see \code{\link[base]{list.files}})
+#' @param pattern Regex pattern for identifying files to include (see [base::list.files()])
 #'
 #' @return Dataframe of compiled data, excluding any rows with all NA values.
 #' @details Also tests for missing values in critical fields.
 #' @export
-#' @import tidyr dplyr
-#' @importFrom purrr map_dfr
-#' @importFrom testthat test_that expect_false
-#' @importFrom rlang .data
-#' @importFrom here here
 
 compile_PC_dat <- function(dir, pattern = '.csv') {
   # get names of all files in directory that match pattern
-  file_list <- list.files(here::here(dir), pattern = pattern,
-                          full.names = TRUE)
+  file_list <- list.files(dir, pattern = pattern, full.names = TRUE)
 
   # compile master data set (assuming CADC Project Leader format)
-  dat <- map_dfr(file_list,
-                 ~read_csv(.x,
-                           col_types = 'cccccnDtttncnccccccclcccc'))
-  dat <- dat %>%
-    filter_all(any_vars(!is.na(.))) %>%
-    mutate_at(vars(.data$Project:.data$Protocol,
-                   .data$`Time Bin`,
-                   .data$Spp:.data$`Breeding Status`,
-                   .data$Researcher:.data$`Data Status`),
-              as.factor)
+  dat <- purrr::map_dfr(
+    file_list,
+    ~readr::read_csv(.x, col_types = 'cccccnDtttncnccccccclcccc')) |> 
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::any_of(
+          c('Project', 'Study Area', 'Transect', 'Point', 'Protocol', 
+            'Time Bin', 'Spp', 'Common Name', 'Scientific Name', 
+            'Detection Cue', 'Distance Bin ID', 'Distance Bin', 
+            'Breeding Status', 'Researcher', 'Data Status')), as.factor))
 
-  testthat::test_that('Missing values in critical fields', {
-    testthat::expect_false(any(is.na(dat$Project)))
-    testthat::expect_false(any(is.na(dat$`Study Area`)))
-    testthat::expect_false(any(is.na(dat$Transect)))
-    testthat::expect_false(any(is.na(dat$Point)))
-    testthat::expect_false(any(is.na(dat$Protocol)))
-    testthat::expect_false(any(is.na(dat$Spp)))
-    testthat::expect_false(any(is.na(dat$`Distance Bin ID`)))
-    testthat::expect_false(any(is.na(dat$Count)))
-  })
+  # check for NA values in critical fields:
+  critfields <- dat |> 
+    dplyr::select(
+      dplyr::any_of(c('Project', 'Study Area', 'Transect', 'Point', 'Protocol', 
+                    'Spp', 'Distance_Bin_ID', 'Count')))
+  rows_with_na = !stats::complete.cases(critfields)
+  
+  if(length(rows_with_na) > 0) {
+    warning(length(rows_with_na), 
+            ' rows contain NA values in one or more critical fields')
+  }
   return(dat)
 }
