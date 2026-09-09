3734577a1722954fb40f616cd44f4c8a9ffcaee0
Kristen Dybala <kdybala@gmail.com>
2026-09-08 17:53:50 -0700

    update to base pipe operator; remove redundant step to "complete" visits

--- R/summarize_PC_dat.R
+++ R/summarize_PC_dat.R
@@ -1,4 +1,6 @@
 #' Format and summarize point count survey data for a given species and project code.
+#' 
+#' Returns the total count (regardless of detection distance) for the selected species at each unique combination of Visit, Point, Transect, and Year in the data, including zero counts where the species was not detected.
 #'
 #' @param df Data frame of point count data, with the standard field names as
 #' in the format downloaded from CADC Project Leader. Filter to include only
@@ -6,47 +8,45 @@
 #' @param species Character string for the 4-letter species code, in all caps
 #' @param project Character string for the 4-letter project code(s), in all caps
 #'
-#' @return Dataframe containing the total count (regardless of detection
-#' distance) for the selected species at each unique combination of Visit,
-#' Point, Transect, and Year in the data, including zero counts where the
-#' species was not detected.
-#' @export
-#' @import dplyr
+#' @return Dataframe 
+#' 
 #' @importFrom rlang .data
+#' @export
 #'
+
 summarize_PC_dat <- function(df, species, project) {
 
   # format data to include surveys where count is zero,
   # summarize total Count at each unique visit across distance bins
-  df %>%
-    format_PC_dat(species, project) %>%
-    group_by(.data$Spp, .data$Project, .data$Transect, .data$Point, .data$Year,
-             .data$Visit) %>%
-    summarize(Count = sum(.data$Count)) %>%
-    ungroup()
+  df |>
+    format_PC_dat(species, project) |>
+    dplyr::group_by(.data$Spp, .data$Project, .data$Transect, .data$Point, 
+                    .data$Year, .data$Visit) |>
+    dplyr::summarize(Count = sum(.data$Count)) |>
+    dplyr::ungroup()
 }
 
 format_PC_dat <- function(df, species, project) {
   # filter projects, add Year field & select relevant columns
-  df <- df %>%
-    filter(.data$Project %in% project) %>%
-    mutate(Year = as.numeric(format(.data$Date, '%Y'))) %>%
-    select(.data$Project, .data$Transect, .data$Point, .data$Year, .data$Visit,
-           .data$Spp, .data$Count, .data$mindist, .data$maxdist)
+  sdf <- df |>
+    dplyr::filter(.data$Project %in% project) |>
+    dplyr::mutate(Year = as.numeric(format(.data$Date, '%Y')),
+                  Spp = as.character(.data$Spp)) |>
+    dplyr::select(
+      dplyr::any_of(c('Project', 'Transect', 'Point', 'Year', 'Visit', 'Spp',
+                      'Count', 'mindist', 'maxdist')))
 
   # generate complete list of all unique surveys in the Project (regardless of
   # species), then join to subset containing species of interest; ensure data is
   # 'complete' with all surveys listed even if count is zero
-  df %>%
-    select(.data$Project, .data$Transect, .data$Point, .data$Year, .data$Visit) %>%
-    distinct() %>%
-    left_join(df %>% filter(.data$Spp == species),
-              by = c('Project', 'Transect', 'Point', 'Year', 'Visit')) %>%
-    mutate(Spp = replace_na(.data$Spp, species)) %>%
-    complete(.data$Spp,
-             nesting(.data$Project, .data$Transect, .data$Point, .data$Year, .data$Visit)) %>%
-    # filter(!is.na(.data$Spp)) %>%
-    mutate(Count = case_when(is.na(.data$Count) ~ 0,
-                             TRUE ~ .data$Count)) %>%
-    mutate_at(vars(.data$Project:.data$Point, .data$Spp), as.factor)
+  sdf |>
+    dplyr::select(
+      dplyr::any_of(c('Project', 'Transect', 'Point', 'Year', 'Visit'))) |>
+    dplyr::distinct() |>
+    dplyr::left_join(
+      sdf |> dplyr::filter(.data$Spp == species),
+      by = c('Project', 'Transect', 'Point', 'Year', 'Visit')) |>
+    dplyr::mutate(Spp = tidyr::replace_na(.data$Spp, species),
+                  Spp = as.factor(.data$Spp),
+                  Count = dplyr::if_else(is.na(.data$Count), 0, .data$Count))
 }
