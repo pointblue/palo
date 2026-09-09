9cb0c6a25f0fbeb4f3ce8c640a34d977515e3508
Kristen Dybala <kdybala@gmail.com>
2026-09-08 17:48:26 -0700

    update to base pipe operator; include support and documentation for rjags and MCMCvis as suggested but not required dependencies; make plotting and printing outputs optional

--- R/fit_BBS_model.R
+++ R/fit_BBS_model.R
@@ -1,4 +1,8 @@
 #' Fit BBS-style hierarchical model for estimating trends in point count survey data
+#' 
+#' Uses \pkg{rjags} to fit a hierarchical model to point count data to estimate trends in a single species' abundance over time, with support for simultaneous estimation of separate overall trends for one or more "project" areas (e.g. POGO vs. PINN), with each project including many point count stations grouped into transects. Models include random intercepts for year within project, transects, and points. Optionally, models can also include a term for fitting overdispersion. Install \pkg{rjags} with: \code{install.packages("rjags")}
+#' 
+#' If plot = TRUE or print = TRUE, uses \pkg{MCMCvis} to plot traceplots and print a results summary table. Install it with: \code{install.packages("MCMCvis")}
 #'
 #' @param inputdata Inputdata created by running \code{\link{setup_BBS_model}}
 #' @param n.adapt Number of iterations for adaptation, defaults to 1000
@@ -7,6 +11,8 @@
 #' @param n.chains Number of MCMC chains, defaults to 3
 #' @param overdispersion Defaults to TRUE, to include a term in the model for
 #' fitting overdispersion, as in BBS models
+#' @param print if TRUE, print table summarizing results
+#' @param plot if TRUE, plot traceplots
 #' @param ... Additional arguments passed to \code{\link[rjags]{jags.model}},
 #' e.g. initial values.
 #'
@@ -14,16 +20,32 @@
 #' statistics from MCMCsummary and prints trace plots from MCMCplot.
 #'
 #' @export
-#' @import rjags
-#' @importFrom stats update
-#' @importFrom MCMCvis MCMCsummary MCMCtrace
-#'
 
 fit_BBS_model <- function(inputdata, n.adapt = 1000, n.burnin = 5000,
                           n.sample = 10000, n.chains = 3,
-                          overdispersion = TRUE,
+                          overdispersion = TRUE, print = TRUE, plot = TRUE,
                           ...) {
 
+  if (!requireNamespace("rjags", quietly = TRUE)) {
+    stop(
+      "Package 'rjags' is required for fit_BBS_model(). ",
+      "Install it with install.packages('rjags').",
+      "You may also need to install JAGS from: https://mcmc-jags.sourceforge.io/",
+      call. = FALSE
+    )
+  }
+  
+  if (plot | print) {
+    if (!requireNamespace("MCMCvis", quietly = TRUE)) {
+      stop(
+        "Package 'MCMCvis' is required for fit_BBS_model() with plot = TRUE or print = TRUE. ",
+        "Install it with install.packages('MCMCvis').",
+        call. = FALSE
+      )
+    }
+  }
+  
+  
   if (overdispersion == TRUE) {
       vars = c('intercept', 'slope', 'sigma.year', 'sigma.transect',
                'sigma.point', 'sigma.overdispersion', 'index', 'trend')
@@ -156,34 +178,32 @@ fit_BBS_model <- function(inputdata, n.adapt = 1000, n.burnin = 5000,
   }"
   }
 
-  # results = runjags::run.jags(model = modelstring,
-  #                             monitor = vars,
-  #                             data = inputdata[-which(names(inputdata) %in% c('year.pred', 'dat'))],
-  #                             n.chains = n.chains,
-  #                             adapt = n.adapt,
-  #                             burnin = n.burnin,
-  #                             sample = n.sample,
-  #                             modules = c('glm'),
-  #                             method = method,
-  #                             ...)
-
-  load.module('glm')
+  rjags::load.module('glm')
 
   jm = rjags::jags.model(file = textConnection(modelstring),
                          data = inputdata[-which(names(inputdata) %in% c('year.pred', 'dat'))],
                          n.adapt = n.adapt,
                          n.chains = n.chains,
                          ...)
-  update(jm, n.iter = n.burnin)
-  results = rjags::coda.samples(jm, variable.names = vars, n.iter = n.sample, thin = 1)
+  
+  stats::update(jm, n.iter = n.burnin)
+  
+  results = rjags::coda.samples(jm, variable.names = vars, n.iter = n.sample,
+                                thin = 1)
 
-  MCMCvis::MCMCsummary(results,
-                       params = vars[-which(vars %in% c('index', 'trend'))]) %>%
-    print()
-
-  MCMCvis::MCMCtrace(results,
-                     params = vars[-which(vars %in% c('index', 'trend'))],
-                     pdf = FALSE)
+  if (print) {
+    MCMCvis::MCMCsummary(
+      results,
+      params = vars[-which(vars %in% c('index', 'trend'))]) |> 
+      print()
+  }
+  
+  if (plot) {
+    MCMCvis::MCMCtrace(
+      results,
+      params = vars[-which(vars %in% c('index', 'trend'))], pdf = FALSE)
+  }
+  
 
   return(results)
 }
