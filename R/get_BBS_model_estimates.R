0da8272bb5906431cb3c9e71f6df1f12ebc35145
Kristen Dybala <kdybala@gmail.com>
2026-09-08 17:52:00 -0700

    update to base pipe operator; add support and documentation for MCMCvis as required for this function

--- R/get_BBS_model_estimates.R
+++ R/get_BBS_model_estimates.R
@@ -1,4 +1,6 @@
 #' Extract annual trend or abundance index estimates from BBS-style hierarchical model
+#' 
+#' Uses \pkg{MCMCvis} to summarize posterior distributions. Install it with: \code{install.packages("MCMCvis")}
 #'
 #' @param modresults Model output from running \code{\link{fit_BBS_model}}
 #' @param type Type of estimates to extract: annual indices of abundance
@@ -12,35 +14,44 @@
 #' For type = 'trend', provide the full list of years between the earliest and last.
 #' @return Dataframe containing predicted value and 95% credible interval for each year
 #' @export
-#' @importFrom MCMCvis MCMCpstr
-#' @importFrom stats quantile
-#' @importFrom dplyr %>%
 #'
-get_BBS_model_estimates <- function(modresults, type, projects = NULL, years = NULL) {
+get_BBS_model_estimates <- function(modresults, type, projects = NULL, 
+                                    years = NULL) {
+  
+  if (!requireNamespace("MCMCvis", quietly = TRUE)) {
+    stop(
+      "Package 'MCMCvis' is required for fit_BBS_model() with plot = TRUE or print = TRUE. ",
+      "Install it with install.packages('MCMCvis').",
+      call. = FALSE
+    )
+  }
 
   est <- MCMCvis::MCMCpstr(modresults, params = type,
                           func = function(x) {
-                            quantile(x, probs = c(0.5, 0.025, 0.975))
+                            stats::quantile(x, probs = c(0.5, 0.025, 0.975))
                           })[[1]]
 
   if (type == 'slope') {
     dimnames(est)[[2]] <- c('median', 'lower', 'upper')
-    res <- as.data.frame(est) %>%
-      mutate(type = type)
+    res <- as.data.frame(est) |> 
+      dplyr::mutate(type = type)
 
   } else if (type %in% c('index', 'trend')) {
     dimnames(est)[[2]] <- projects
 
-    res <- as.data.frame(est) %>%
-      mutate(Year = years) %>%
-      pivot_longer(-.data$Year, names_to = 'variable', values_to = 'value') %>%
-      separate(.data$variable, into = c('Project', 'variable'), sep = 4, fill = 'right') %>%
-      mutate(variable = case_when(variable == '.50%' ~ 'median',
-                                  variable == '.2.5%' ~ 'lower',
-                                  variable == '.97.5%' ~ 'upper')) %>%
-      pivot_wider(names_from = 'variable', values_from = 'value') %>%
-      arrange(.data$Project, .data$Year) %>%
-      mutate(type = type)
+    res <- as.data.frame(est) |> 
+      dplyr::mutate(Year = years) |> 
+      tidyr::pivot_longer(-'Year', names_to = 'variable', 
+                          values_to = 'value') |> 
+      tidyr::separate('variable', into = c('Project', 'variable'), 
+                      sep = 4, fill = 'right') |> 
+      dplyr::mutate(
+        variable = dplyr::case_when(variable == '.50%' ~ 'median',
+                                    variable == '.2.5%' ~ 'lower',
+                                    variable == '.97.5%' ~ 'upper')) |> 
+      tidyr::pivot_wider(names_from = 'variable', values_from = 'value') |> 
+      dplyr::arrange(dplyr::any_of(c('Project', 'Year'))) |> 
+      dplyr::mutate(type = type)
   }
 
   return(res)
