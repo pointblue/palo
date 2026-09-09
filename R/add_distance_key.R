6d553e238cfc0468805cf9f0b62544199fe52258
Kristen Dybala <kdybala@gmail.com>
2026-09-08 17:46:38 -0700

    update to base pipe operator

--- R/add_distance_key.R
+++ R/add_distance_key.R
@@ -2,26 +2,28 @@
 #'
 #' @param dat Dataframe containing the field 'Distance Bin'
 #'
-#' @return Dataframe
+#' @return tibble
 #' @export
-#' @importFrom rlang .data
 
 add_distance_key <- function(dat) {
-  key <- dat %>%
-    select(.data$Protocol, .data$`Distance Bin ID`, .data$`Distance Bin`) %>%
-    distinct() %>%
-    mutate(mindist = case_when(grepl('>', `Distance Bin`) ~
-                                 gsub('>', '', `Distance Bin`),
-                               grepl('<', `Distance Bin`) ~ '0',
-                               grepl('\\sto\\s', `Distance Bin`) ~
-                                 gsub(' to .*$', '', `Distance Bin`),
-                               `Distance Bin` == 'FlyOver' ~ NA_character_),
-           maxdist = case_when(grepl('>', `Distance Bin`) ~ NA_character_,
-                               grepl('<', `Distance Bin`) ~
-                                 gsub('<', '', `Distance Bin`),
-                               grepl('\\sto\\s', `Distance Bin`) ~
-                                 gsub('^.* to ', '', `Distance Bin`),
-                               `Distance Bin` == 'FlyOver' ~ NA_character_)) %>%
-    mutate_at(vars(.data$mindist:.data$maxdist), as.numeric)
-  left_join(dat, key, by = c('Protocol', 'Distance Bin ID', 'Distance Bin'))
+  key <- dat |> 
+    dplyr::select('Protocol', 'Distance Bin ID', 'Distance Bin') |> 
+    dplyr::distinct() |>
+    dplyr::mutate(
+      mindist = dplyr::case_when(
+        grepl('>', 'Distance Bin') ~ gsub('>', '', 'Distance Bin'),
+        grepl('<', 'Distance Bin') ~ '0',
+        grepl('\\sto\\s', 'Distance Bin') ~ gsub(' to .*$', '', 'Distance Bin'),
+        'Distance Bin' == 'FlyOver' ~ NA_character_),
+      maxdist = dplyr::case_when(
+        grepl('>', 'Distance Bin') ~ NA_character_, 
+        grepl('<', 'Distance Bin') ~ gsub('<', '', 'Distance Bin'),
+        grepl('\\sto\\s', 'Distance Bin') ~ gsub('^.* to ', '', 'Distance Bin'),
+        'Distance Bin' == 'FlyOver' ~ NA_character_)) |>
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::any_of(c('mindist', 'maxdist')), as.numeric))
+  
+  dplyr::left_join(dat, key, 
+                  by = c('Protocol', 'Distance Bin ID', 'Distance Bin'))
 }
